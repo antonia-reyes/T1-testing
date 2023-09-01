4006bd80ebcb708bbd44fcbb3112a2561904ecd4
cristobalfr <cristobalfr@uc.cl>
2023-09-01 00:30:23 -0400

    listo assertTrue

--- core/rules/assertion_true.py
+++ core/rules/assertion_true.py
@@ -1,3 +1,5 @@
+from _ast import Assign, Name
+from typing import Any
 from ..rule import *
 import ast
 from core.rules.warning import *
@@ -5,39 +7,33 @@ import inspect
 
 class AssertionTrueVisitor(WarningNodeVisitor):
     #  Implementar Clase
-    # def __init__(self):
-    #     pass
+    def __init__(self):
+        super().__init__()
+        self.variables = {}
+    
+    def visit_Assign(self, node: Assign):
+        if type(node.value) == Constant:
+            print('ASSIGN')
+            print('VARAIBLE')
+            print(node.targets[0].id)
+            print('VALOR')
+            print(node.value.value)
+            self.variables[node.targets[0].id] = node.value.value
+            # NodeVisitor.generic_visit(self, node)
 
-    def visit_Call(self, node):
-        # ARGUMENTOS DE LLAMADA A FUNCION
-        print('ARGS')
-        print(node.args[0])
-        print(type(node.args[0]))
-        # SELF
-        print('SELF')
-        print(node.func.value.id)
-        # NOMBRE DE FUNCION
-        print('FUNC')
-        print(node.func.attr)
-        if type(node.args[0]) is ast.Constant:
-            if node.func.attr == 'assertTrue' and node.args[0].value == True:
-                print('assert true detected')
-                # Get lines of function
-                # lines, lineno = inspect.getsourcelines(node.lineno)
-                # print(lines)
-                # print(lineno)
-                # GET LINE OF CALL
-                self.addWarning('AssertTrueWarning', node.lineno, 'useless assert true detected')
-        else:
-            if node.func.attr == 'assertTrue':
-                print('assert true detected')
-                # Get lines of function
-                # lines, lineno = inspect.getsourcelines(node.__class__)
-                # print(lines)
-                # print(lineno)
-                # GET LINE OF CALL
-                self.addWarning('AssertTrueWarning', node.lineno, 'useless assert true detected')
-        NodeVisitor.generic_visit(self, node)
+    def visit_Call(self, node: Call):
+        if type(node.func) is ast.Attribute:
+            if type(node.args[0]) is ast.Constant:
+                if node.func.attr == 'assertTrue' and node.args[0].value == True:
+                    print('assert true detected 1')
+                    self.addWarning('AssertTrueWarning', node.lineno, 'useless assert true detected')
+            else:
+                if node.func.attr == 'assertTrue':
+                    print('assert true detected 2')
+                    if node.args[0].id in self.variables:
+                        if self.variables[node.args[0].id] == True:
+                            self.addWarning('AssertTrueWarning', node.lineno, 'useless assert true detected')
+        # NodeVisitor.generic_visit(self, node)
 
 
 class AssertionTrueTestRule(Rule):
