391e74e80dace4b3a9eba5ebef252c1c822c3f21
antonia-reyes <antonia.reyes@uc.cl>
2023-09-04 22:38:28 -0300

    var inline 2/4

--- core/transformers/inline_rewriter.py
+++ core/transformers/inline_rewriter.py
@@ -34,6 +34,10 @@ class ValueVariableVisitor(NodeVisitor):
                             self.variables[self.currentFunction.name][target.id] = [valor_variable[0], node.lineno]
                 if isinstance(node.value, Constant):
                     self.variables[self.currentFunction.name][node.targets[0].id] = [node.value.value, node.lineno]
+                if isinstance(node.value, Call):
+                    if isinstance(node.value.func, Name):
+                        function_name = node.value.func.id
+                        self.variables[self.currentFunction.name][target.id] = [node.value, node.lineno]
 
     def visit_Call(self, node: Call):
         if type(node.func) is ast.Attribute:
@@ -51,9 +55,24 @@ class ValueVariableVisitor(NodeVisitor):
 class VariableTransformer(NodeTransformer):
     def __init__(self, nodes):
         super().__init__()
-        self.nodes = nodes
+        self.variables = nodes
 
+    def visit_Assign(self, node: Assign):
+        for target in node.targets:
+            if isinstance(target, Name):
+                #print(target.id) # variable (por ej x   y   z  )
+                llave = list(self.variables.keys())[0]
+                if target.id in self.variables[llave]:
+                    return
+    
     def visit_Name(self, node):
+        llave = list(self.variables.keys())[0]
+        if isinstance(node, Name) and node.id in self.variables[llave]:
+            value = self.variables[llave][node.id][0]
+            if isinstance(value, ast.Call):
+                node = value
+                return node
+            return Constant(value=value, kind=None)
         return node
 
 
@@ -63,6 +82,9 @@ class InlineCommand(RewriterCommand):
         visitor = ValueVariableVisitor()
         visitor.visit(ast)
         nodes = visitor.check()
+        print("--------------- VARIABLES -----------------")
+        print(nodes)
+        print("--------------- VARIABLES -----------------")
         transformer = VariableTransformer(nodes)
         new_tree = fix_missing_locations(transformer.visit(ast))
         return new_tree
